d43edfe732fc8c2c5111f299ef5f5cb6e65ca737
Tayna Alves <taynaalro@gmail.com>
2018-02-07 13:56:16 -0200

    voluntar

---
+++ home.js
@@ -0,0 +1,67 @@
+// SLIDE DE FRASES INICIO
+
+// let carro1 = document.querySelectorAll('.subtituloinicio');
+//   let slider = 0;
+//     function avancarSlide1(){
+//       carro1[slider].style.display = 'none';
+//         slider++;
+//     if(slider >= carro1.length){
+//               slider = 0;
+//     }
+//       carro1[slider].style.display = 'block';
+//     }
+//     setInterval(avancarSlide1, 3500);
+//     avancarSlide1();
+
+// LETRAS DIGITANDO ESPAÇO ONGS
+
+// let frase1 = 'Um projeto criado para ajestáSdo. Um projeto criado para ajestáSdo Um projeto criado para ajestáSdo';
+//       let f1 = document.querySelector('.teste');
+//       let contador = 0;
+//       function exibirLetra(){
+//         let letra = frase1[contador];
+//         f1.innerHTML += letra;
+//         contador++;
+//         if(contador >= frase1.length){
+//           clearInterval(intervalo);
+          
+          // f1.innerHTML = '';
+          // contador = 0;
+        
+      //   }
+
+      // }
+      // let intervalo = setInterval(exibirLetra, 130);
+      // clearInterval(intervalo);
+
+// CONTROLE DE TIME CARROSSEL ONGS
+
+$('.carousel').carousel({
+  interval: 3500
+});
+
+// $(window).scroll(function () {
+//   if ($(this).scrollTop() < 300) {
+//       $('.navbar').css("background-color", "#red")
+//   } else {
+//       $('.navbar').css("background-color", "brown")
+//   }
+// });      
+
+
+// var x = 0;
+// var y = 0;
+// var z = 0;
+
+// function myMoveFunction() {
+//     document.getElementById("demo").innerHTML = z+=1;
+// }
+
+// function myEnterFunction() {
+//     document.getElementById("demo2").innerHTML = x+=1;
+// }
+
+// function myOverFunction() {
+//     document.getElementById("demo3").innerHTML = y+=1;
+// }
+
